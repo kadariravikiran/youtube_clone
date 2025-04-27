0915d3716cb9e22ea075ca58a86c048b71e65fc4
Nethikar Hemalatha <nethikarhemalatha@gmail.com>
2025-04-27 13:35:30 +0530

    Fix port binding for Render

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allows access from any IP
+    port: process.env.PORT || 3000, // Use Render's port or fallback to 3000
+  },
   preview: {
-    allowedHosts: ['youtube-clone-4-ogwk.onrender.com']
+    allowedHosts: ['youtube-clone-4-ogwk.onrender.com'],
   }
 });
