a3e15ce8cc89d40c8b09bd5cc72afc49b9647e8d
Nethikar Hemalatha <nethikarhemalatha@gmail.com>
2025-04-27 13:29:28 +0530

    Allow preview host in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: process.env.PORT || 4173,  // Default port or the port specified in the environment variable
-    host: '0.0.0.0',  // Ensures the server is accessible externally
-  },
+  preview: {
+    allowedHosts: ['youtube-clone-4-ogwk.onrender.com']
+  }
 });
