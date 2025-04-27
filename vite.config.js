ec5ec066a2392d1807a79b263e373a3bf222dacd
Nethikar Hemalatha <nethikarhemalatha@gmail.com>
2025-04-27 13:14:34 +0530

    Updated Vite config for port binding

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 4173,  // Default port or the port specified in the environment variable
+    host: '0.0.0.0',  // Ensures the server is accessible externally
+  },
+});
